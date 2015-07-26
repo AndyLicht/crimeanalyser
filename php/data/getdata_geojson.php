a1c26f83d85496fa4121a266836cc40e8c75bbbc
Tim Balschmiter <tb.rostock@gmail.com>
2015-07-26 12:20:11 +0200

    anpassungen bei der Info ausgabe

--- php/data/getdata_geojson.php
+++ php/data/getdata_geojson.php
@@ -25,7 +25,7 @@
 	    \"extent\":[".$bbox."]
           }
         },\"features\": [";
-	$results = pg_query($dbconn, "SELECT oid,object,time,to_char(time,'ww') as week,ST_X(the_geom),ST_Y(the_geom) as Standort FROM ".$_GET['machinename'].";");
+	$results = pg_query($dbconn, "SELECT oid,object,time,to_char(time,'ww') as week,ST_X(the_geom),ST_Y(the_geom),location,pdescription FROM ".$_GET['machinename'].";");
 	$results_count = pg_num_rows($results);
 	$i = 0;
 	while ($row = pg_fetch_row($results))
@@ -33,11 +33,45 @@
 	    $i++;
 	    if($i < $results_count)
 	    {
-		$geojson = $geojson."{\"id\":".$row[0].",\"type\": \"Feature\",\"properties\":{\"time\":\"".$row[2]."\",\"week\":\"".$row[3]."\",\"object\":\"".$row[1]."\"},\"geometry\": {\"type\": \"Point\",\"coordinates\": [".$row[4].",".$row[5]."]}},";
+		$geojson = $geojson."
+			    {
+				\"id\":".$row[0].",
+				\"type\": \"Feature\",
+				\"properties\":
+				{
+				    \"time\":\"".$row[2]."\",
+				    \"week\":\"".$row[3]."\",
+				    \"object\":\"".$row[1]."\",
+				    \"location\":\"".$row[6]."\",
+				    \"beschreibung\":\"".$row[7]."\"
+				},
+				\"geometry\": 
+				{
+				    \"type\": \"Point\",
+				    \"coordinates\": [".$row[4].",".$row[5]."]
+				}
+			    },";
 	    }
 	    else
 	    {
-		$geojson = $geojson."{\"id\":".$row[0].",\"type\": \"Feature\",\"properties\":{\"time\":\"".$row[2]."\",\"week\":\"".$row[3]."\",\"object\":\"".$row[1]."\"},\"geometry\": {\"type\": \"Point\",\"coordinates\": [".$row[4].",".$row[5]."]}}]}";
+		$geojson = $geojson."
+			    {
+				\"id\":".$row[0].",
+				\"type\": \"Feature\",
+				\"properties\":
+				{
+				    \"time\":\"".$row[2]."\",
+				    \"week\":\"".$row[3]."\",
+				    \"object\":\"".$row[1]."\",
+				    \"location\":\"".$row[6]."\",
+				    \"beschreibung\":\"".$row[7]."\"
+				},
+				\"geometry\": 
+				{
+				    \"type\": \"Point\",
+				    \"coordinates\": [".$row[4].",".$row[5]."]
+				}
+			    }]}";
 	    }
 	}
     }
